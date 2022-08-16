e345a4495047d539f589b787c18c31f34a3a2640
JordanTownshend <91408554+JordanTownshend@users.noreply.github.com>
2022-08-17 00:56:39 +1200

    db.js gets from env variable now

--- config/db.js
+++ config/db.js
@@ -1,11 +1,9 @@
 const mongoose = require('mongoose');
-const config = require('config');
-const db = config.get('mongoURI');
 
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      db,
+      process.env.MONGODB_URI,
       {
         useNewUrlParser: true
       }
